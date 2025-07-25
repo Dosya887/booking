765005c252d4d77637e4bdac38780380ca16fbd5
Dosya887 <tdastan.777@mail.ru>
2025-07-25 17:35:11 +0600

    fix

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('register/', views.user_register, name='register'),
     path('single/<int:product_id>/', views.single_view, name='single_view'),
+    path('category/<int:category_id>/', views.category_view, name='category'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -7,18 +7,25 @@ from .forms import CustomUserCreationForm
 from .models import Product, Category
 
 
-
-
-
 def index(request):
     categories = Category.objects.all()
     products = Product.objects.all()
-    return render(
-        request= request,
-        template_name='main/index.html',
-        context={'products':products,
-                 'categories':categories}
-    )
+    return render(request, 'main/index.html', {
+        'categories': categories,
+        'products': products,
+        'selected_category': None,  # Для выделения активной кнопки
+    })
+
+def category_view(request, category_id):
+    categories = Category.objects.all()
+    selected_category = get_object_or_404(Category, id=category_id)
+    products = Product.objects.filter(category=selected_category)
+    return render(request, 'main/index.html', {
+        'categories': categories,
+        'products': products,
+        'selected_category': selected_category,  # Покажем активную категорию
+    })
+
 
 def single_view(request, product_id):
     product = get_object_or_404(Product, id=product_id)
